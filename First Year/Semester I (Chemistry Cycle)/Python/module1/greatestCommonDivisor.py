8f87d82cdd74194fe18cd93c9e875ed2332989c1
saqib hussain <hussainsakib44@gmail.com>
2023-05-15 07:29:20 +0530

    Update greatestCommonDivisor.py

--- First Year/Semester I (Chemistry Cycle)/Python/module1/greatestCommonDivisor.py
+++ First Year/Semester I (Chemistry Cycle)/Python/module1/greatestCommonDivisor.py
@@ -7,18 +7,19 @@ numb1 = int(input('Enter the first number: '))
 numb2 = int(input('Enter the second number: '))
 
 #Method 1;
-def gcd(a, b):
-  if (a > b):
-    smaller = b
-  else:
-    smaller = a
-  i = int(smaller/2)
-  while (i > 0):
-            if (a%i == 0 and b%i == 0):
-                print(i, 'is the H.C.F')
-                break
-            else:
-                i -= 1
+def gcd(a,b):
+    smaller = b if a >= b else a
+    if a % smaller == 0 and b % smaller == 0:
+        print(smaller, 'is the H.C.F')
+        return
+    i = 1
+    n = smaller//2
+    while i > 0:
+        if a%i == 0 and b%i == 0:
+            print(i, 'is the H.C.F')
+            break 
+        else:
+            i -= 1
 
 
 gcd(numb1, numb2)
